17f31e0d6ed9037b3287fc5e14eb356295fbaf96
Joel Speed <joel.speed@hotmail.co.uk>
2021-07-02 11:38:47 +0100

    Ensure CCM pods all run before CNI components

--- pkg/cloud/cloud_test.go
+++ pkg/cloud/cloud_test.go
@@ -1,6 +1,7 @@
 package cloud
 
 import (
+	"strings"
 	"testing"
 
 	configv1 "github.com/openshift/api/config/v1"
@@ -8,6 +9,8 @@ import (
 	"github.com/openshift/cluster-cloud-controller-manager-operator/pkg/cloud/azure"
 	"github.com/openshift/cluster-cloud-controller-manager-operator/pkg/cloud/openstack"
 	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -134,5 +137,122 @@ func TestGetBootstrapResources(t *testing.T) {
 				assert.NotEqualValues(t, resources, newResources)
 			}
 		})
+	}
+}
+
+func TestResourcesRunBeforeCNI(t *testing.T) {
+	/*
+		As CNI relies on CMM to initialist the Node IP addresses. We must ensure
+		that CCM pods can run before the CNO has been deployed and before the CNI
+		initialises the Node.
+
+		To achieve this, we must tolerate the not-ready taint, use host
+		networking and use the internal API Load Balancer instead of the API Service.
+	*/
+
+	platforms := []configv1.PlatformType{
+		configv1.AWSPlatformType,
+		configv1.OpenStackPlatformType,
+		configv1.GCPPlatformType,
+		configv1.AzurePlatformType,
+		configv1.VSpherePlatformType,
+		configv1.OvirtPlatformType,
+		configv1.IBMCloudPlatformType,
+		configv1.LibvirtPlatformType,
+		configv1.KubevirtPlatformType,
+		configv1.BareMetalPlatformType,
+		configv1.NonePlatformType,
+	}
+	for _, platform := range platforms {
+		t.Run(string(platform), func(t *testing.T) {
+			resources := GetResources(platform)
+			resources = append(resources, GetBootstrapResources(platform)...)
+
+			for _, resource := range resources {
+				switch obj := resource.(type) {
+				case *corev1.Pod:
+					checkResourceRunsBeforeCNI(t, obj.Spec)
+				case *appsv1.Deployment:
+					checkResourceRunsBeforeCNI(t, obj.Spec.Template.Spec)
+				case *appsv1.DaemonSet:
+					checkResourceRunsBeforeCNI(t, obj.Spec.Template.Spec)
+				default:
+					// Nothing to check for non
+				}
+			}
+		})
+	}
+}
+
+func checkResourceRunsBeforeCNI(t *testing.T, podSpec corev1.PodSpec) {
+	checkResourceTolerations(t, podSpec)
+	checkHostNetwork(t, podSpec)
+	checkVolumes(t, podSpec)
+	checkContainerCommand(t, podSpec)
+}
+
+func checkResourceTolerations(t *testing.T, podSpec corev1.PodSpec) {
+	uninitializedTaint := corev1.Toleration{
+		Key:      "node.cloudprovider.kubernetes.io/uninitialized",
+		Operator: corev1.TolerationOpExists,
+		Effect:   corev1.TaintEffectNoSchedule,
+	}
+	notReadyTaint := corev1.Toleration{
+		Key:      "node.kubernetes.io/not-ready",
+		Operator: corev1.TolerationOpExists,
+		Effect:   corev1.TaintEffectNoSchedule,
+	}
+
+	tolerations := podSpec.Tolerations
+	assert.Contains(t, tolerations, uninitializedTaint, "PodSpec should tolerate the uninitialized taint")
+	assert.Contains(t, tolerations, notReadyTaint, "PodSpec should tolerate the not-ready taint")
+}
+
+func checkHostNetwork(t *testing.T, podSpec corev1.PodSpec) {
+	assert.Equal(t, podSpec.HostNetwork, true, "PodSpec should set HostNetwork true")
+}
+
+func checkVolumes(t *testing.T, podSpec corev1.PodSpec) {
+	directory := corev1.HostPathDirectory
+	hostVolume := corev1.Volume{
+		Name: "host-etc-kube",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: "/etc/kubernetes",
+				Type: &directory,
+			},
+		},
+	}
+	hostVolumeMount := corev1.VolumeMount{
+		MountPath: "/etc/kubernetes",
+		Name:      "host-etc-kube",
+		ReadOnly:  true,
+	}
+
+	assert.Contains(t, podSpec.Volumes, hostVolume, "PodSpec Volumes should contain host-etc-kube host path volume")
+
+	for _, container := range podSpec.Containers {
+		assert.Contains(t, container.VolumeMounts, hostVolumeMount, "Container VolumeMounts should contain host-etc-kube volume mount")
+	}
+}
+
+func checkContainerCommand(t *testing.T, podSpec corev1.PodSpec) {
+	binBash := "/bin/bash"
+	dashC := "-c"
+	setAPIEnv := `#!/bin/bash
+set -o allexport
+if [[ -f /etc/kubernetes/apiserver-url.env ]]; then
+  source /etc/kubernetes/apiserver-url.env
+else
+  URL_ONLY_KUBECONFIG=/etc/kubernetes/kubeconfig
+fi
+exec `
+
+	for _, container := range podSpec.Containers {
+		command := container.Command
+		assert.Len(t, command, 3, "Container Command should have 3 elements")
+		assert.Equal(t, command[0], binBash, "Container Command first element should equal %q", binBash)
+		assert.Equal(t, command[1], dashC, "Container Command second element should equal %q", dashC)
+		assert.True(t, strings.HasPrefix(command[2], setAPIEnv), "Container Command third (%q) element should start with %q", command[2], setAPIEnv)
 	}
 }
