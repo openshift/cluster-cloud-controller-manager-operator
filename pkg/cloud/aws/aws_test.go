9fc7b791d40d0d8fdccc47f2d7010dc13455a8d3
Mike Fedosin <mfedosin@redhat.com>
2021-07-08 16:14:19 +0200

    Remove "render" binary
    With https://github.com/openshift/cluster-cloud-controller-manager-operator/pull/76
    we don't need to render static pod manifests on the bootstrap machine.
    
    So, "render" binary and all related code can be removed from the
    operator.

--- pkg/cloud/aws/aws_test.go
+++ pkg/cloud/aws/aws_test.go
@@ -19,17 +19,3 @@ func TestGetResources(t *testing.T) {
 	assert.Contains(t, names, "aws-cloud-controller-manager")
 	assert.Contains(t, kinds, "Deployment")
 }
-
-func TestGetBootstrapResources(t *testing.T) {
-	resources := GetBootstrapResources()
-	assert.Len(t, resources, 1)
-
-	var names, kinds []string
-	for _, r := range resources {
-		names = append(names, r.GetName())
-		kinds = append(kinds, r.GetObjectKind().GroupVersionKind().Kind)
-	}
-
-	assert.Contains(t, names, "aws-cloud-controller-manager")
-	assert.Contains(t, kinds, "Pod")
-}
