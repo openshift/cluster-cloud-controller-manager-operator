392dc16f96a908a1627a7a276d16d766eb84c42d
Danil-Grigorev <dgrigore@redhat.com>
2021-07-16 10:53:28 +0200

    Ensure verify diff errors out only in CI

--- hack/verify-diff.sh
+++ hack/verify-diff.sh
@@ -3,7 +3,11 @@ FILE_DIFF=$(git ls-files -o --exclude-standard)
 if [ "$FILE_DIFF" != "" ]; then
   echo "Found untracked files:"
   echo $FILE_DIFF
-  exit 1
 fi
 
-git diff --exit-code
+if [ "$OPENSHIFT_CI" == "true" ]; then
+  if [ "$FILE_DIFF" != "" ]; then
+    exit 1
+  fi
+  git diff --exit-code
+fi
