b8e5afc72082affbfa488a8c38c7b0b50cedc4b9
ehila <ehila@redhat.com>
2021-12-17 01:32:40 -0500

    fix: cloud platform tests
    removed unused emptylogger struct
    updated toggle for turning on and off klog logging
    
    Signed-off-by: ehila <ehila@redhat.com>

--- pkg/util/testingutils/logging.go
+++ pkg/util/testingutils/logging.go
@@ -3,47 +3,14 @@ package testingutils
 import (
 	"github.com/go-logr/logr"
 	"k8s.io/klog/v2"
-	"k8s.io/klog/v2/klogr"
 )
 
 // TurnOffKlog supresses klog. Intended to use only in tests.
 func TurnOffKlog() {
-	klog.SetLogger(logr.New(&EmptyLogger{}))
+	klog.SetLogger(logr.Discard())
 }
 
+// TurnOnKlog remove the logger and to set back to defaults
 func TurnOnKlog() {
-	klog.Flush()
-	klog.SetLogger(klogr.New())
-}
-
-// EmptyLogger implements logr.Logging
-type EmptyLogger struct{}
-
-// Enabled always returns false
-func (e *EmptyLogger) Init(info logr.RuntimeInfo) {}
-
-// Enabled always returns false
-func (e *EmptyLogger) Enabled(level int) bool {
-	return false
-}
-
-// Info does nothing
-func (e *EmptyLogger) Info(level int, msg string, keysAndValues ...interface{}) {}
-
-// Error does nothing
-func (e *EmptyLogger) Error(err error, msg string, keysAndValues ...interface{}) {}
-
-// V returns itself
-func (e *EmptyLogger) V(level int) logr.LogSink {
-	return e
-}
-
-// WithValues returns itself
-func (e *EmptyLogger) WithValues(keysAndValues ...interface{}) logr.LogSink {
-	return e
-}
-
-// WithName returns itself
-func (e *EmptyLogger) WithName(name string) logr.LogSink {
-	return e
+	klog.ClearLogger()
 }
