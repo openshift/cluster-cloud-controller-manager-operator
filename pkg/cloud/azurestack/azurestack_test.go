2c922b7e582c71ed2ff55b09b481524ed7f75ca6
michael mccune <msm@opbstudios.com>
2023-10-25 20:31:52 -0400

    add a cloud config transformer for azure stack
    This change creates a CloudConfigTransformer for Azure Stack cloud
    provider. The transformer will unmarshal json from the cloud config into
    a `Config` struct from the azure cloud provider and then update the the
    `VMType` field if it is unset. This is being added because Azure has
    updated the default vm type to value that cannot be added to the basic
    load balancers which are associated with Azure Stack.
    
    The change also adds a test suite to accompany the transformer.

--- pkg/cloud/azurestack/azurestack_test.go
+++ pkg/cloud/azurestack/azurestack_test.go
@@ -1,13 +1,23 @@
 package azurestack
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
+	. "github.com/onsi/gomega"
 	configv1 "github.com/openshift/api/config/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	azure "sigs.k8s.io/cloud-provider-azure/pkg/provider"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/openshift/cluster-cloud-controller-manager-operator/pkg/config"
+)
+
+const (
+	infraCloudConfName = "test-config"
+	infraCloudConfKey  = "foo"
 )
 
 func TestResourcesRenderingSmoke(t *testing.T) {
@@ -59,6 +69,91 @@ func TestResourcesRenderingSmoke(t *testing.T) {
 
 			resources := assets.GetRenderedResources()
 			assert.Len(t, resources, 2)
+		})
+	}
+}
+
+func makeInfrastructureResource(platform configv1.PlatformType, cloudName configv1.AzureCloudEnvironment) *configv1.Infrastructure {
+	cfg := configv1.Infrastructure{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster",
+		},
+		Status: configv1.InfrastructureStatus{
+			PlatformStatus: &configv1.PlatformStatus{
+				Type: platform,
+			},
+		},
+		Spec: configv1.InfrastructureSpec{
+			CloudConfig: configv1.ConfigMapFileReference{
+				Name: infraCloudConfName,
+				Key:  infraCloudConfKey,
+			},
+			PlatformSpec: configv1.PlatformSpec{
+				Type: platform,
+			},
+		},
+	}
+
+	if platform == configv1.AzurePlatformType {
+		cfg.Status.PlatformStatus.Azure = &configv1.AzurePlatformStatus{
+			CloudName: cloudName,
+		}
+	}
+
+	return &cfg
+}
+
+// This test is a little complicated with all the JSON marshalling and
+// unmarshalling, but it is necessary due to the nature of how this data
+// is stored in Kuberenetes. The ConfigMaps containing the cloud config
+// will have string encoded JSON objects in them, due to the non-deterministic
+// natue of map object in Go we will need to examine the data instead of
+// comparing strings.
+func TestCloudConfigTransformer(t *testing.T) {
+	tc := []struct {
+		name     string
+		source   azure.Config
+		expected azure.Config
+		infra    *configv1.Infrastructure
+		errMsg   string
+	}{
+		{
+			name:     "Azure Stack Hub sets the vmType to standard",
+			source:   azure.Config{},
+			expected: azure.Config{VMType: "standard"},
+			infra:    makeInfrastructureResource(configv1.AzurePlatformType, configv1.AzureStackCloud),
+		},
+		{
+			name:     "Azure Stack Hub doesn't modify vmType if user set",
+			source:   azure.Config{VMType: "vmss"},
+			expected: azure.Config{VMType: "vmss"},
+			infra:    makeInfrastructureResource(configv1.AzurePlatformType, configv1.AzureStackCloud),
+		},
+		{
+			name:   "Non Azure Stack Hub returns an error",
+			source: azure.Config{},
+			infra:  makeInfrastructureResource(configv1.AzurePlatformType, configv1.AzurePublicCloud),
+			errMsg: fmt.Sprintf("invalid platform, expected CloudName to be %s", configv1.AzureStackCloud),
+		},
+	}
+
+	for _, tc := range tc {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+
+			src, err := json.Marshal(tc.source)
+			g.Expect(err).NotTo(HaveOccurred(), "Marshal of source data should succeed")
+
+			actual, err := CloudConfigTransformer(string(src), tc.infra, nil)
+			if tc.errMsg != "" {
+				g.Expect(err).Should(MatchError(tc.errMsg))
+				g.Expect(actual).Should(Equal(""))
+			} else {
+				var observed azure.Config
+				g.Expect(json.Unmarshal([]byte(actual), &observed)).To(Succeed(), "Unmarshal of observed data should succeed")
+
+				g.Expect(observed).Should(Equal(tc.expected))
+			}
 		})
 	}
 }
