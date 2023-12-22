64e61fe7f8981680ea4bbeccdb42cad7d5c19586
Theo Barber-Bany <theobarberbany@gmail.com>
2023-12-22 15:11:04 +0000

    Updates Azure CloudConfigTransformer to set cloud
    This change adds the functionality from the cluster-config-operator
    azure CloudConfigTransformer.
    
    The functionality we bring over is:
    
    1. Ensure that the Cloud is set in the cloud.conf
       i. If it is set, verify that it is valid and does not conflict with the
          infrastructure config. If it conflicts, we want to error
      ii. If it is not set, default to public cloud (configv1.AzurePublicCloud)
    
     2. Verify the cloud name set in the infra config is valid, if it is not
     bail with an informative error
    
    See https://github.com/openshift/cluster-config-operator/pull/133 for
    the PR that initially added this functionality.

--- pkg/cloud/azure/azure.go
+++ pkg/cloud/azure/azure.go
@@ -4,10 +4,12 @@ import (
 	"embed"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/asaskevich/govalidator"
 	configv1 "github.com/openshift/api/config/v1"
 	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	azureconsts "sigs.k8s.io/cloud-provider-azure/pkg/consts"
@@ -26,6 +28,24 @@ var (
 		{ReferenceObject: &appsv1.Deployment{}, EmbedFsPath: "assets/cloud-controller-manager-deployment.yaml"},
 		{ReferenceObject: &appsv1.DaemonSet{}, EmbedFsPath: "assets/cloud-node-manager-daemonset.yaml"},
 	}
+)
+
+var (
+	validAzureCloudNames = map[configv1.AzureCloudEnvironment]bool{
+		configv1.AzurePublicCloud:       true,
+		configv1.AzureUSGovernmentCloud: true,
+		configv1.AzureChinaCloud:        true,
+		configv1.AzureGermanCloud:       true,
+		configv1.AzureStackCloud:        true,
+	}
+
+	validAzureCloudNameValues = func() []string {
+		v := make([]string, 0, len(validAzureCloudNames))
+		for n := range validAzureCloudNames {
+			v = append(v, string(n))
+		}
+		return v
+	}()
 )
 
 type imagesReference struct {
@@ -91,11 +111,59 @@ func NewProviderAssets(config config.OperatorConfig) (common.CloudProviderAssets
 	return assets, nil
 }
 
+func IsAzure(infra *configv1.Infrastructure) bool {
+	if infra.Status.PlatformStatus != nil &&
+		infra.Status.PlatformStatus.Type == configv1.AzurePlatformType {
+		return true
+	}
+	return false
+}
+
 func CloudConfigTransformer(source string, infra *configv1.Infrastructure, network *configv1.Network) (string, error) {
+	if !IsAzure(infra) {
+		return "", fmt.Errorf("invalid platform, expected to be Azure")
+	}
+
 	var cfg azure.Config
 	if err := json.Unmarshal([]byte(source), &cfg); err != nil {
 		return "", fmt.Errorf("failed to unmarshal the cloud.conf: %w", err)
 	}
+
+	// We are copying the behaviour from CCO's transformer we need to:
+	// 1. Ensure that the Cloud is set in the cloud.conf
+	//   i. If it is set, verify that it is valid and does not conflict with the
+	//      infrastructure config. If it conflicts, we want to error
+	//  ii. If it is not set, default to public cloud (configv1.AzurePublicCloud)
+	//
+	// 2. Verify the cloud name set in the infra config is valid, if it is not
+	// bail with an informative error
+
+	// Verify the cloud name set in the infra config is valid
+	cloud := configv1.AzurePublicCloud
+	if azurePlatform := infra.Status.PlatformStatus.Azure; azurePlatform != nil {
+		if c := azurePlatform.CloudName; c != "" {
+			if !validAzureCloudNames[c] {
+				return "", field.NotSupported(field.NewPath("status", "platformStatus", "azure", "cloudName"), c, validAzureCloudNameValues)
+			}
+			cloud = c
+		}
+	}
+
+	// Ensure Cloud is set in cloud.conf matches what is set in infra
+	if cfg.Cloud != "" {
+		if !strings.EqualFold(string(cloud), cfg.Cloud) {
+			return "",
+				fmt.Errorf(`invalid user-provided cloud.conf: \"cloud\" field in user-provided
+				cloud.conf conflicts with infrastructure object`)
+		}
+	}
+
+	// TODO: Remove when you work this out (before merging)
+	// Why is cfg.Cloud not typed: type AzureCloudEnvironment string
+
+	// At this point these should always be the same
+	// comparrison
+	cfg.Cloud = string(cloud)
 
 	// If the virtual machine type is not set we need to make sure it uses the
 	// "standard" instance type. See OCPBUGS-25483 and OCPBUGS-20213 for more
