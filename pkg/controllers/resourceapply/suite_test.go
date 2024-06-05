8ec3ccf914705ab8707b13fdda4cd354705a41bf
elmiko <msm@opbstudios.com>
2024-06-05 15:34:25 -0400

    refactor resourceapply tests
    This change converts the resourceapply tests to a style that uses the
    Ginkgo testing framework for control structures. Primarily the standard
    library style of looping array test is replaced with DescribeTable based
    ginkgo tests.
    
    A few "TODO" comments have been left in places where it was apparent
    there were plans for future tests, mostly around errors, and were not
    implemented.
    
    Further, some tests have had portions of their data refactored. In
    specific, the tests that use the `simpleSecret` and `simpleConfigMap`
    functions were all using the same values. It appeared that future work
    might have been planned here, but these have been factored out in favor
    of simplicity.

---
+++ pkg/controllers/resourceapply/suite_test.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2023 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourceapply
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2/textlogger"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/komega"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+var ctx = context.Background()
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "ResourceApply Suite")
+}
+
+var _ = BeforeSuite(func() {
+	var err error
+	logf.SetLogger(textlogger.NewLogger(textlogger.NewConfig()))
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{}
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	// +kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	komega.SetClient(k8sClient)
+	komega.SetContext(ctx)
+})
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
