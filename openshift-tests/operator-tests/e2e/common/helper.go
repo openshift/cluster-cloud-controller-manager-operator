b8d07aa0902ae71d133830ad04834228d07873e5
vr4manta <vr4manta@gmail.com>
2026-04-14 11:46:45 -0400

    Reverted client logic for now

--- openshift-tests/operator-tests/e2e/common/helper.go
+++ openshift-tests/operator-tests/e2e/common/helper.go
@@ -3,12 +3,15 @@ package common
 import (
 	ginkgov2 "github.com/onsi/ginkgo/v2"
 	"k8s.io/client-go/rest"
-	"k8s.io/kubernetes/test/e2e/framework"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
 // NewClientConfigForTest returns a config configured to connect to the api server
 func NewClientConfigForTest() (*rest.Config, error) {
-	config, err := framework.LoadConfig()
+	loader := clientcmd.NewDefaultClientConfigLoadingRules()
+	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loader, &clientcmd.ConfigOverrides{ClusterInfo: api.Cluster{InsecureSkipTLSVerify: true}})
+	config, err := clientConfig.ClientConfig()
 	if err == nil {
 		ginkgov2.GinkgoLogr.Info("Found configuration for", "host", config.Host)
 	}
